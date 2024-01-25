8f40c556993f81fbb68a13421af2a0f3720c1893
Anukul Prakash Saxena <anukulsaxena83@gmail.com>
2024-01-25 19:30:14 +0530

    edit : playlist page

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -5,24 +5,14 @@ import conf from '../conf/conf';
 import movieService from '../render/movieconfig';
 
 function Playlist() {
-    function handleClick() {
-        console.log('ok');
-    }
-
-
-
-
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [])
     return (
-        <div className=' min-h-screen dark:bg-neutral-700 w-full text-center'>
-            {/* <Spinner height='h-96' /> */}
-            <button className='mt-40 p-3 bg-white rounded-lg' onClick={handleClick}>
-                name
-            </button>
-
+        <div className=' min-h-screen dark:bg-neutral-700 text-white w-full text-center'>
+            <Spinner height='h-96' />
+            Coming Soon
         </div>
     )
 }
